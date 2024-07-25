a8648a7ee03a4ed8a067b126c7ff010b9f69dbbd
Jonathan <shady14@hotmail.fr>
2024-07-25 10:59:01 +0200

    modify description of function

--- _print_rev.c
+++ _print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *print_rev- print string in reverse
+ *_print_rev - print string in reverse
  *Description: reverse string
  *@s: pointer
  *Return: void
@@ -11,6 +11,7 @@ void _print_rev(char *s)
 {
 	int x = 0;
 	int y;
+
 	while (s[x] != '\0')
 	{
 		x++;
